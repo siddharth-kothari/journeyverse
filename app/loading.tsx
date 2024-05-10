6aea6b7d155e20beb750aefc2e3f35f7c9493eec
Siddharth Kothari <sidkothari005@gmail.com>
2024-05-10 22:12:29 +0530

    minor changes

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,15 +1,11 @@
-import loading from './../assets/loading.gif'
-import Image from 'next/image'
+import loading from "./../assets/loading.gif";
+import Image from "next/image";
 
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return (
-        <div className="absolute z-20 top-0 grid w-[100vw] h-[100vh] bg-black place-content-center">
-            <Image
-            className=''
-                src={loading}
-                alt="Loading..."
-            />
-        </div>
-    )
-  }
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="absolute z-20 top-0 grid w-[100vw] h-[100vh] bg-black/80 place-content-center">
+      <Image className="" src={loading} alt="Loading..." />
+    </div>
+  );
+}
