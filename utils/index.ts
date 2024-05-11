876c9275cae67e96462e18b4caf9b277d1f17a28
Siddharth Kothari <sidkothari005@gmail.com>
2024-05-11 08:59:11 +0530

    bug fixes v4

--- utils/index.ts
+++ utils/index.ts
@@ -1,29 +1,27 @@
 import { IPost } from "@/typing";
-import { serialize } from "next-mdx-remote/serialize";
+// import { serialize } from "next-mdx-remote/serialize";
 
+export const debounce = (fn: () => void, timeout = 300) => {
+  let timer: NodeJS.Timeout;
 
-export const debounce = (fn:()=>void,timeout = 300) => {
-    let timer: NodeJS.Timeout;
+  const debounced = (...args: any) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, timeout);
+  };
 
-    const debounced = (...args:any) =>{
-        clearTimeout(timer);
-        timer = setTimeout(()=>{
-            fn.apply(this, args);
-        }, timeout)
-    }
+  return debounced;
+};
 
+// export const serializeMarkdown = async ( item : IPost) => {
+//     const Content = await serialize(item.attributes.Content as string);
 
-    return debounced;
-} 
-
-export const serializeMarkdown = async ( item : IPost) => {
-    const Content = await serialize(item.attributes.Content as string);
-
-    return {
-        ...item,
-        attributes:{
-            ...item.attributes,
-            Content,
-        }
-    }
-}
+//     return {
+//         ...item,
+//         attributes:{
+//             ...item.attributes,
+//             Content,
+//         }
+//     }
+// }
