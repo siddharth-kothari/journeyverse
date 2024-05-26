5f33f619d9b322704ae788833fd50027a89cfac7
Siddharth Kothari <sidkothari005@gmail.com>
2024-05-26 23:45:31 +0530

    Added content editor bug fix v2

--- components/BlogPostForm.tsx
+++ components/BlogPostForm.tsx
@@ -2,12 +2,27 @@
 
 import { useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
 
-// Dynamically import FroalaEditor and its plugins
-const FroalaEditor = dynamic(() => import("react-froala-wysiwyg"), {
-  ssr: false,
-});
+import FroalaEditor from "react-froala-wysiwyg";
+import "froala-editor/js/plugins/image.min.js";
+import "froala-editor/js/plugins/font_family.min.js";
+import "froala-editor/js/plugins/char_counter.min.js";
+import "froala-editor/js/plugins/align.min.js";
+import "froala-editor/js/plugins/colors.min.js";
+import "froala-editor/js/plugins/emoticons.min.js";
+import "froala-editor/js/plugins/font_size.min.js";
+import "froala-editor/js/plugins/line_height.min.js";
+import "froala-editor/js/plugins/link.min.js";
+import "froala-editor/js/plugins/lists.min.js";
+import "froala-editor/js/plugins/paragraph_format.min.js";
+import "froala-editor/js/plugins/quote.min.js";
+import "froala-editor/js/plugins/paragraph_style.min.js";
+import "froala-editor/js/plugins/special_characters.min.js";
+import "froala-editor/js/third_party/spell_checker.min.js";
+import "froala-editor/js/plugins/table.min.js";
+import "froala-editor/js/plugins/table.min.js";
+import "froala-editor/css/froala_style.min.css";
+import "froala-editor/css/froala_editor.pkgd.min.css";
 
 interface Errors {
   title?: string;
@@ -29,30 +44,6 @@ const BlogPostForm = ({ initialData, categories }: any) => {
       setIsClient(true);
     }
   }, []);
-
-  useEffect(() => {
-    if (isClient) {
-      // Dynamically import Froala plugins on the client side
-      import("froala-editor/js/plugins/image.min.js");
-      import("froala-editor/js/plugins/font_family.min.js");
-      import("froala-editor/js/plugins/char_counter.min.js");
-      import("froala-editor/js/plugins/align.min.js");
-      import("froala-editor/js/plugins/colors.min.js");
-      import("froala-editor/js/plugins/emoticons.min.js");
-      import("froala-editor/js/plugins/font_size.min.js");
-      import("froala-editor/js/plugins/line_height.min.js");
-      import("froala-editor/js/plugins/link.min.js");
-      import("froala-editor/js/plugins/lists.min.js");
-      import("froala-editor/js/plugins/paragraph_format.min.js");
-      import("froala-editor/js/plugins/quote.min.js");
-      import("froala-editor/js/plugins/paragraph_style.min.js");
-      import("froala-editor/js/plugins/special_characters.min.js");
-      import("froala-editor/js/third_party/spell_checker.min.js");
-      import("froala-editor/js/plugins/table.min.js");
-      import("froala-editor/css/froala_style.min.css");
-      import("froala-editor/css/froala_editor.pkgd.min.css");
-    }
-  }, [isClient]);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
